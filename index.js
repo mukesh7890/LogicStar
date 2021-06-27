8368943aa63211c29993c3a43aa575d740994129
mukesh7890 <indiancom03@gmail.com>
2021-06-28 00:28:39 +0530

    Simple star printing

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-for (let i = 0; i <= 5; i++) {
-    document.write(i)
-}
+//Test//
+// for (let i = 0; i <= 5; i++) {
+//     document.write(i);
+// }
+//Test//
 
+            //Star Print//
+//Simple star line
+    // for (let i = 1; i <= 5; i++) {
+    //     document.write("*");
+    // }
+
+//Portrait Star line 
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 1; j++){
+            document.write("*");
+        }
+        
+    }
