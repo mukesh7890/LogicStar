d77d92a7ae94be1974b1acb99754a88b035e828d
mukesh7890 <indiancom03@gmail.com>
2021-06-28 14:29:25 +0530

    portrait star print

--- index.js
+++ index.js
@@ -11,9 +11,9 @@
     // }
 
 //Portrait Star line 
-    for (let i = 1; i <= 5; i++) {
-        for (let j = 1; j <= 1; j++){
-            document.write("*");
-        }
-        
-    }
+    // for (let i = 1; i <= 5; i++) {
+    //     for (let j = 1; j <= 1; j++){
+    //         document.write("*");
+    //     }
+    //     document.write("<br>");
+    // }
